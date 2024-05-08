33e7c220d0c61234ada4d4892d67831d15aeea0d
Ali Ghalambor <75223461+alirezaghl@users.noreply.github.com>
2024-05-08 02:35:11 -0700

    Update MLP.py

--- VIT/MLP.py
+++ VIT/MLP.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 class MLP(nn.Module):
   def __init__(self, embed_dimension, hidden_dimension, dropout=0.0):
     super().__init__()
-    
     self.fc1 = nn.Linear(embed_dimension, hidden_dimension)
     self.fc2 = nn.Linear(hidden_dimension, embed_dimension)
     self.activation = nn.GELU()
@@ -12,10 +11,3 @@ class MLP(nn.Module):
   
   def forward(self, x):
     return self.dropout(self.fc2(self.dropout(self.activation(self.fc1(x)))))
-
-
-  def forward(self, x):
-        input = self.norm_1(x)
-        x = x + self.attention(input, input, input)[0]
-        output = x + self.mlp(self.norm_2(x))
-        return output
